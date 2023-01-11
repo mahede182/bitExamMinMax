9fb1d416fe2db36b75d5bbcd024325f30c7cf0f7
Mahede hasan <53116487+mahede182@users.noreply.github.com>
2023-01-11 13:37:42 +0600

    Update minMax.js
    final

--- minMax.js
+++ minMax.js
@@ -3,11 +3,12 @@
 function minMaximum(){
     var data = [10,1000,1,59.10,120,4,-1,30];
 
-    // we can use js built in Math()
-    
-    var min = Math.min(...data)
-    var max = Math.max(...data)
-
+    let max = array[0], min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        // larger number
+        if (array[i] > max) { max = array[i]; }
+        // smaller number
+        if (array[i] < min) { min = array[i]; }
     console.log("minimux is:",min)
     console.log("maximum is:",max)
 }
